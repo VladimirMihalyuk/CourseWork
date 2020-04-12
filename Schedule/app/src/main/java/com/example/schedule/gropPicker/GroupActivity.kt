1b8d9bd77136088d65ce8fc21e9fb08a001fd7db
VladimirMihalyuk <37588348+VladimirMihalyuk@users.noreply.github.com>
2020-04-12 19:13:47 +0300

    Added loading of group and courses

--- Schedule/app/src/main/java/com/example/schedule/gropPicker/GroupActivity.kt
+++ Schedule/app/src/main/java/com/example/schedule/gropPicker/GroupActivity.kt
@@ -2,32 +2,77 @@ package com.example.schedule.gropPicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.schedule.R
-import com.example.schedule.app.ScheduleApplication
-import com.example.schedule.firestore.FireStore
-import com.example.schedule.firestore.Group
-import com.example.schedule.firestore.Lesson
-import java.lang.Exception
-import javax.inject.Inject
+import android.view.View
+import android.widget.AdapterView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
-class GroupActivity : AppCompatActivity() {
+import com.example.schedule.app.ScheduleApplication
+import javax.inject.Inject
+import android.widget.ArrayAdapter
+import com.example.schedule.app.getGroupString
+import kotlinx.android.synthetic.main.activity_group.*
+
+
+
+
+
+
+
+
+class GroupActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     @Inject
-    lateinit var fireStore: FireStore
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    lateinit var viewModel: GroupViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_group)
+        setContentView(com.example.schedule.R.layout.activity_group)
 
         (application as ScheduleApplication).appComponent.inject(this)
 
-        fireStore.getGroups({list: List<Group> -> Log.d("WTF", "$list")},
-            {exception: Exception -> Log.d("WTF", "$exception") })
+        viewModel =ViewModelProvider(this, viewModelFactory).get(GroupViewModel::class.java)
 
-        fireStore.getAllLessons(
-            Group(3,  "TP", 13),
-            {list: List<Lesson> -> Log.d("WTF", "$list")},
-            {exception: Exception -> Log.d("WTF", "$exception") })
+
+
+        viewModel.map.observe(this, Observer { map ->
+            course.adapter = makeAdapter(map.keys.toList().map{"$it курс" })
+            if(map.keys.firstOrNull() != null){
+                group.adapter =
+                    makeAdapter(map[map.keys.firstOrNull() ?: 0]?.
+                        map { it.getGroupString()} ?: listOf())
+            }
+        })
+
+        course.onItemSelectedListener = this
+
+        viewModel.getGroups()
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+        group.adapter = makeAdapter(viewModel.getGroupsForCourse(pos).
+            map { it.getGroupString()})
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {}
+
+    private fun makeAdapter(list: List<String>): ArrayAdapter<String>{
+        val arrayAdapter =
+            ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                list)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        return arrayAdapter
+    }
+
+    fun startLoading(){
+        constraintLayout.visibility = View.INVISIBLE
+        progressBar.visibility = View.VISIBLE
+    }
+
+    fun stopLoading(){
+        constraintLayout.visibility = View.VISIBLE
+        progressBar.visibility = View.INVISIBLE
     }
 }
--- Schedule/app/src/main/java/com/example/schedule/gropPicker/GroupViewModel.kt
+++ Schedule/app/src/main/java/com/example/schedule/gropPicker/GroupViewModel.kt
@@ -1,14 +1,35 @@
 package com.example.schedule.gropPicker
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.schedule.app.SharedPreferencesWrapper
 import com.example.schedule.firestore.FireStore
+import com.example.schedule.firestore.Group
+import com.example.schedule.firestore.Lesson
 import javax.inject.Inject
 
 class GroupViewModel @Inject constructor(
     private val db: FireStore, private val spWrapper: SharedPreferencesWrapper): ViewModel(){
 
 
+    private val _map = MutableLiveData<Map<Int, List<Group>>>()
+    val map: LiveData<Map<Int, List<Group>>>
+        get() = _map
 
+    fun getGroups(){
+        db.getGroups({list: List<Group> -> processList(list)}, {})
+    }
 
+    private fun processList(list: List<Group>){
+           _map.value = list.groupBy { it.course }
+    }
+
+    fun getGroupsForCourse(course: Int):List<Group>{
+        val key = _map.value?.keys?.toList()?.get(course)
+        key?.let{
+            return  _map.value?.get(key) ?: listOf()
+        }
+        return listOf()
+    }
 }
